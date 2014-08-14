e0204188dc34b4dd7af842f9f523b0d8864ce639
MavCite <mdubourg@cite-musique.fr>
2014-08-14 16:30:47 +0200

    Revision config et fichiers sass

--- _integration/config.rb
+++ _integration/config.rb
@@ -1,12 +1,14 @@
 # Require any additional compass plugins here.
+# Extension de mixin html5 venant de html5 boilerplate
 require 'compass-h5bp'
 
 # Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "css"
-sass_dir = "_integration/sass"
-images_dir = "img"
+http_path = "../"
+css_dir = "../css"
+sass_dir = "sass"
+images_dir = "../img"
 javascripts_dir = "js"
+fonts_dir = "../css/fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
